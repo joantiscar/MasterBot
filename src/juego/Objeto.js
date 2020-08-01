6c329088b2c3f9002a536d111fcba79f35dd696e
Joan <joantiscar@gmail.com>
2020-08-02 00:53:10 +0200

    Creadas clases Personaje y Objeto

---
+++ src/juego/Objeto.js
@@ -0,0 +1,5 @@
+class Objeto {
+	tipo = "" // TODO talvez seria mejor crear clases que hereden OBJETO para cada tipo
+	peso = ""
+	nombre = ""
+}
---
+++ src/juego/Personaje.js
@@ -0,0 +1,35 @@
+class Personaje {
+	fuerza = 10
+	destreza = 10
+	constitucion = 10
+	inteligencia = 10
+	sabiduria = 10
+	carisma = 10
+	competencias = {
+		armas: [],
+		armaduras: [],
+		idiomas: [],
+		habilidades: [],
+		utensilios: [],
+		objetos: [],
+	}
+	inventario = []
+	equipo = {
+		cabeza: [],
+		cuello: [],
+		hombros: [],
+		pecho: [],
+		cintura: [],
+		piernas: [],
+		pies: [],
+		brazos: [],
+		manoDerecha: [],
+		manoIzquierda: [],
+		anilloDerecho: [],
+		anilloIzquierdo: [],
+	}
+	equipar () {}
+	desequipar () {}
+
+
+}
